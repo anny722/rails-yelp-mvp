23c9c12be4c7a5444997b68fa7442d21f46ec060
Ana Negura <ana_n21@yahoo.com>
2019-05-22 14:43:48 +0200

    first version

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
+# frozen_string_literal: true
+
+# class comment
 class ApplicationController < ActionController::Base
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -7,5 +7,6 @@ class Restaurant < ApplicationRecord
   validates :name, presence: true
   validates :address, presence: true
   validates :category, presence: true
-  validates :category, inclusion: { in: CATEGORIES }
+  validates :phone_number, presence: true
+  validates :category, inclusion: { in: CATEGORIES }, case_sensitive: false
 end
